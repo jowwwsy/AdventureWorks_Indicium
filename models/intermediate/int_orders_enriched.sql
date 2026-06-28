4612ed799b40d997ded5cc636bf216beac91e281
Joselyne Banegas <joselyne.banegas@indicium.ai>
2026-06-28 22:55:42 +0000

    add fct_sales

--- models/intermediate/int_orders_enriched.sql
+++ models/intermediate/int_orders_enriched.sql
@@ -15,10 +15,11 @@ enriched as (
         od.product_id,
         od.special_offer_id,
         od.carrier_tracking_number,
-        od.order_quantity,
+        od.order_qty,
         od.unit_price,
         od.unit_price_discount,
-        od.unit_price * od.unit_price_discount * od.order_quantity as discount_amount
+        od.order_qty*od.unit_price as sales_amount,
+        od.unit_price * od.unit_price_discount * od.order_qty as discount_amount
     from order_headers oh
     left join order_details od
         on od.sales_order_id = oh.sales_order_id
